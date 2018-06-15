855bc8929cd49525732679864d976ee55e5b9f34
htmltest <struev.vs@gmail.com>
2018-06-15 20:50:26 +0300

    balls

--- js/tools.js
+++ js/tools.js
@@ -356,6 +356,11 @@ $(document).ready(function() {
 
     $('.describe-next a').click(function(e) {
         if ($('.describe-textarea textarea').val() != '') {
+            // json с картинками в base64 и текстом
+            var dataTransfer = {
+                "curText": $('.share-textarea textarea').val()
+            };
+
             $('.contest-list').removeClass('contest-list-describe').addClass('contest-list-share');
             $('.contest-step.active').removeClass('active');
             $('.contest-step-4').addClass('active');
@@ -391,13 +396,42 @@ $(document).ready(function() {
                 context.drawImage(imgTheme, 0, 0, canvas.width, canvas.height);
                 $('#social-editor-img').attr('src', canvas.toDataURL('image/jpeg'));
 
-                // json с картинкой в base64 и текстом
-                var dataTransfer = {
-                    "curIMG": $('#social-editor-img').attr('src'),
-                    "curText": $('.share-textarea textarea').val()
-                };
+                dataTransfer["curIMG"] = $('#social-editor-img').attr('src');
             };
             imgTheme.src = $('.share-img-to-social').data('border');
+
+            var canvas2 = document.getElementById('social-editor2');
+            var context2 = canvas2.getContext('2d');
+            context2.clearRect(0, 0, canvas2.width, canvas2.height);
+            context2.fillRect(0, 0, canvas2.width, canvas2.height);
+
+            var newWidth2  = 687 * imgZoom;
+            var newHeight2 = 687 * imgZoom;
+            var newX2 = -343 * imgZoom;
+            var newY2 = -343 * imgZoom;
+            if (imgWidth > imgHeight) {
+                var diffHeight2 = newHeight2 / imgHeight;
+                newWidth2 = imgWidth * diffHeight;
+                newX2 = -(newWidth2 - 687 * imgZoom) / 2 - 343 * imgZoom;
+            } else {
+                var diffWidth2 = newWidth2 / imgWidth;
+                newHeight2 = imgHeight * diffWidth2;
+                newY = -(newHeight2 - 687 * imgZoom) / 2 - 343 * imgZoom;
+            }
+
+            context2.save();
+            context2.translate(343 + imgLeft + 197, 343 + imgTop + 198);
+            context2.rotate((exifOrientation + imgRotate) * TO_RADIANS);
+            context2.drawImage(img, newX2, newY2, newWidth2, newHeight2);
+            context2.restore();
+            var imgTheme2 = new Image();
+            imgTheme2.onload = function() {
+                context2.drawImage(imgTheme2, 0, 0, canvas2.width, canvas2.height);
+                $('#social-editor-img2').attr('src', canvas2.toDataURL('image/jpeg'));
+
+                dataTransfer["curIMG2"] = $('#social-editor-img2').attr('src');
+            };
+            imgTheme2.src = $('.share-img-to-social').data('border2');
 
         } else {
             alert('Необходимо ввести текст');
@@ -409,6 +443,38 @@ $(document).ready(function() {
         $('.share-link').removeClass('open');
         $(this).parent().addClass('open');
         e.preventDefault();
+    });
+
+    $('body').on('click', '.rating-item-info', function(e) {
+        var curLink = $(this);
+        windowOpen(curLink.attr('href'));
+        e.preventDefault();
+    });
+
+    $('body').on('keyup', function(e) {
+        if (e.keyCode == 27) {
+            windowClose();
+        }
+    });
+
+    $(document).click(function(e) {
+        if ($(e.target).hasClass('window')) {
+            windowClose();
+        }
+    });
+
+    $(window).resize(function() {
+        windowPosition();
+    });
+
+    $('body').on('click', '.window-close', function(e) {
+        windowClose();
+        e.preventDefault();
+    });
+
+    var clipboard = new Clipboard('.copy-button');
+    clipboard.on('success', function(e) {
+        alert('Промо-код скопирован в буфер');
     });
 
 });
@@ -427,4 +493,76 @@ function checkErrors() {
             curField.removeClass('valid');
         }
     });
+}
+
+function windowOpen(linkWindow, dataWindow, callbackWindow) {
+    var curPadding = $('.wrapper').width();
+    $('html').addClass('window-open');
+    curPadding = $('.wrapper').width() - curPadding;
+    $('body').css({'margin-right': curPadding + 'px'});
+
+    if ($('.window').length == 0) {
+        $('body').append('<div class="window"><div class="window-loading"></div></div>')
+    }
+
+    $.ajax({
+        type: 'POST',
+        url: linkWindow,
+        dataType: 'html',
+        data: dataWindow,
+        cache: false
+    }).done(function(html) {
+        if ($('.window').length > 0) {
+            $('.window').remove();
+        }
+        $('body').append('<div class="window"><div class="window-loading"></div></div>')
+
+        $('.window').append('<div class="window-container window-container-load"><div class="window-content">' + html + '<a href="#" class="window-close"></a></div></div>')
+
+        if ($('.window-container img').length > 0) {
+            $('.window-container img').each(function() {
+                $(this).attr('src', $(this).attr('src'));
+            });
+            $('.window-container').data('curImg', 0);
+            $('.window-container img').one('load', function() {
+                var curImg = $('.window-container').data('curImg');
+                curImg++;
+                $('.window-container').data('curImg', curImg);
+                if ($('.window-container img').length == curImg) {
+                    $('.window-container').removeClass('window-container-load');
+                    windowPosition();
+                }
+            });
+        } else {
+            $('.window-container').removeClass('window-container-load');
+            windowPosition();
+        }
+
+        if (typeof (callbackWindow) != 'undefined') {
+            callbackWindow.call();
+        }
+
+        $('.window form').each(function() {
+            initForm($(this));
+        });
+    });
+}
+
+function windowPosition() {
+    if ($('.window').length > 0) {
+        $('.window-container').css({'left': '50%', 'margin-left': -$('.window-container').width() / 2});
+
+        $('.window-container').css({'top': '50%', 'margin-top': -$('.window-container').height() / 2, 'padding-bottom': 0});
+        if ($('.window-container').height() > $('.window').height() - 60) {
+            $('.window-container').css({'top': '30px', 'margin-top': 0, 'padding-bottom': 30});
+        }
+    }
+}
+
+function windowClose() {
+    if ($('.window').length > 0) {
+        $('.window').remove();
+        $('html').removeClass('window-open');
+        $('body').css({'margin-right': 0});
+    }
 }
