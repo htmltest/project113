8f1f7b149badb579f689f4fffb824c595ac19ff7
htmltest <struev.vs@gmail.com>
2018-06-17 16:56:32 +0300

    game

--- js/tools.js
+++ js/tools.js
@@ -477,6 +477,266 @@ $(document).ready(function() {
         alert('Промо-код скопирован в буфер');
     });
 
+    if ($('.game').length > 0) {
+        var canvas = document.getElementById('game-finish-editor');
+        var context = canvas.getContext('2d');
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.fillRect(0, 0, canvas.width, canvas.height);
+
+        var imgTheme = new Image();
+        imgTheme.onload = function() {
+            context.drawImage(imgTheme, 0, 0, canvas.width, canvas.height);
+        };
+        imgTheme.src = $('.game-finish-images').data('border');
+
+        var canvas2 = document.getElementById('game-finish-editor2');
+        var context2 = canvas2.getContext('2d');
+        context2.clearRect(0, 0, canvas2.width, canvas2.height);
+        context2.fillRect(0, 0, canvas2.width, canvas2.height);
+
+        var imgTheme2 = new Image();
+        imgTheme2.onload = function() {
+            context2.drawImage(imgTheme2, 0, 0, canvas2.width, canvas2.height);
+        };
+        imgTheme2.src = $('.game-finish-images').data('border2');
+    }
+
+    var isGaming = false;
+    var curRound = 0;
+    var gameTimeSecondsAll = 0;
+    var gameTimeTotal = 0;
+    var maxRoundTime = 0;
+    var gameTimeSeconds = 0;
+    var gameTimeMinutes = 0;
+    var gameTimeHours = 0;
+    var gameTimer = null;
+    var gameClicks = 0;
+    var gamePair = 0;
+    var gameClearTimer = null;
+
+    var gameResult = {
+        "time1" : 0,
+        "card1" : 0,
+
+        "time2" : 0,
+        "card2" : 0,
+
+        "time3" : 0,
+        "card3" : 0,
+    };
+
+    $('.game-descr-link a').click(function(e) {
+        $('.game-descr').fadeOut(300, function() {
+            $('.game-content').fadeIn(300, function() {
+                curRound = 1;
+                loadImages();
+            });
+        });
+        e.preventDefault();
+    });
+
+    function loadImages() {
+        $('.game-start').hide();
+        $('.game-rounds-image').removeClass('open');
+        var newImages = [];
+        switch(curRound) {
+            case 1:
+                newImages = images1.concat(images1);
+                maxRoundTime = round1Period;
+                break;
+            case 2:
+                newImages = images2.concat(images2);
+                maxRoundTime = round2Period;
+                break;
+            case 3:
+                newImages = images3.concat(images3);
+                maxRoundTime = round3Period;
+                break;
+        }
+        function compareRandom(a, b) {
+            return Math.random() - 0.5;
+        }
+        newImages.sort(compareRandom);
+
+        var loadImagesCount = 0;
+        $('.game-rounds-image img').one('load', function() {
+            loadImagesCount++;
+            if (loadImagesCount == newImages.length) {
+                $('.game-start').show();
+            }
+        });
+
+        for (var i = 0; i < newImages.length; i++) {
+            $('.game-rounds-image').eq(i).find('img').attr('src', newImages[i]);
+        }
+    }
+
+    $('.game-start a').click(function(e) {
+        $('.game-start').hide();
+        $('.game-start').html($('.game-start').data('next'));
+        $('.game-rounds-images').addClass('openall');
+        window.setTimeout(function() {
+            $('.game-rounds-images').removeClass('openall');
+            $('.game-rounds-image').removeClass('open success');
+            isGaming = true;
+            gameTimeSecondsAll = 0;
+            gameTimeSeconds = 0;
+            gameTimeMinutes = 0;
+            gameTimeHours = 0;
+            gameClicks = 0;
+            $('.game-info').hide();
+            $('.game-time .seconds').html('00');
+            $('.game-time .minutes').html('00');
+            $('.game-time .hours').html('00');
+            gameTimer = window.setInterval(function() {
+                gameTimeSecondsAll++;
+                gameTimeSeconds++;
+                if (gameTimeSecondsAll >= maxRoundTime) {
+                    stopRound();
+                }
+                if (gameTimeSeconds >= 60) {
+                    gameTimeSeconds = 0;
+                    gameTimeMinutes++;
+                    if (gameTimeMinutes >= 60) {
+                        gameTimeMinutes = 0;
+                        gameTimeHours++;
+                    }
+                }
+                $('.game-time .seconds').html(gameTimeSeconds);
+                if (gameTimeSeconds < 10) {
+                    $('.game-time .seconds').html('0' + gameTimeSeconds);
+                }
+                $('.game-time .minutes').html(gameTimeMinutes);
+                if (gameTimeMinutes < 10) {
+                    $('.game-time .minutes').html('0' + gameTimeMinutes);
+                }
+                $('.game-time .hours').html(gameTimeHours);
+                if (gameTimeHours < 10) {
+                    $('.game-time .hours').html('0' + gameTimeHours);
+                }
+            }, 1000);
+        }, 2000);
+        e.preventDefault();
+    });
+
+    $('.game-rounds-image').click(function() {
+        if (isGaming) {
+            var curImg = $(this);
+            if (!curImg.hasClass('open')) {
+                gamePair++;
+                if (gamePair == 2) {
+                    gamePair = 0;
+                    var success = false;
+                    $('.game-rounds-image.open img').each(function() {
+                        if ($(this).attr('src') == curImg.find('img').attr('src')) {
+                            success = true;
+                            $(this).parent().addClass('success');
+                            curImg.addClass('success');
+                        }
+                    });
+                    if (!success) {
+                        window.clearTimeout(gameClearTimer);
+                        gameClearTimer = null;
+
+                        gameClearTimer = window.setTimeout(function() {
+                            $('.game-rounds-image.open:not(.success)').removeClass('open');
+                        }, 500);
+                    }
+                }
+                if ($('.game-rounds-image.open:not(.success)').length > 1) {
+                    window.clearTimeout(gameClearTimer);
+                    gameClearTimer = null;
+                    $('.game-rounds-image.open:not(.success)').removeClass('open');
+                }
+                curImg.addClass('open');
+                gameClicks++;
+                $('.game-clicks').html(gameClicks);
+                if ($('.game-rounds-image.success').length == $('.game-rounds-image').length) {
+                    stopRound();
+                }
+            }
+        }
+    });
+
+    function stopRound() {
+        window.clearInterval(gameTimer);
+        gameTimer = null;
+        isGaming = false;
+        switch(curRound) {
+            case 1:
+                gameResult.time1 = gameTimeSecondsAll;
+                gameResult.card1 = $('.game-rounds-image.success').length;
+                $('.game-header-item').removeClass('active');
+                $('.game-header-item').eq(1).addClass('active');
+                $('.game-time .seconds').html('00');
+                $('.game-time .minutes').html('00');
+                $('.game-time .hours').html('00');
+                $('.game-clicks').html('0');
+                break;
+            case 2:
+                gameResult.time2 = gameTimeSecondsAll;
+                gameResult.card2 = $('.game-rounds-image.success').length;
+                $('.game-header-item').removeClass('active');
+                $('.game-header-item').eq(2).addClass('active');
+                $('.game-time .seconds').html('00');
+                $('.game-time .minutes').html('00');
+                $('.game-time .hours').html('00');
+                $('.game-clicks').html('0');
+                break;
+            case 3:
+                gameResult.time3 = gameTimeSecondsAll;
+                gameResult.card3 = $('.game-rounds-image.success').length;
+                $('.game-time .seconds').html('00');
+                $('.game-time .minutes').html('00');
+                $('.game-time .hours').html('00');
+                $('.game-clicks').html('0');
+                break;
+        }
+        $('.game-rounds-stat-item').eq(curRound - 1).find('.game-rounds-stat-item-result-count').html($('.game-rounds-image.success').length);
+        $('.game-rounds-stat-item').eq(curRound - 1).find('.game-rounds-stat-item-result-time').html(gameTimeSecondsAll);
+        $('.game-rounds-stat-item').eq(curRound - 1).addClass('active');
+        var newPoints = Number($('.game-rounds-stat-item-result-points').html());
+        gameTimeTotal += gameTimeSecondsAll;
+        if (gameTimeSecondsAll <= maxRoundTime) {
+            newPoints += maxRoundTime * 10 + (maxRoundTime - gameTimeSecondsAll) * 10;
+        }
+        $('.game-rounds-stat-item-result-points').html(newPoints);
+        $('.game-info-count').html($('.game-rounds-image.success').length);
+        $('.game-info-time').html(gameTimeSecondsAll);
+        $('.game-info').removeClass('game-info-1 game-info-2 game-info-3').addClass('game-info-' + curRound).show();
+        if (curRound < 3) {
+            curRound++;
+            loadImages();
+        } else {
+            $('.game-rounds-ctrl, .game-rounds-images').hide();
+            $('.game-finish').show();
+            var summMinutes = Math.floor(gameTimeTotal / 60);
+            var summSeconds = gameTimeTotal - summMinutes * 60;
+            var gameTimeTotalString = summMinutes + ':' + summSeconds;
+
+            var canvas = document.getElementById('game-finish-editor');
+            var context = canvas.getContext('2d');
+            context.fillStyle = '#ffffff';
+            context.strokeStyle = '#ffffff';
+            context.font = 'bold 64px/70px MyriadPro, sans-serif';
+            context.fillText(gameTimeTotalString, 158, 273);
+            context.fillText(newPoints, 354, 273);
+            $('#game-finish-img').attr('src', canvas.toDataURL('image/jpeg'));
+
+            var canvas2 = document.getElementById('game-finish-editor2');
+            var context2 = canvas2.getContext('2d');
+            context2.fillStyle = '#ffffff';
+            context2.strokeStyle = '#ffffff';
+            context2.font = 'bold 64px/70px MyriadPro, sans-serif';
+            context2.fillText(gameTimeTotalString, 115, 370);
+            context2.fillText(newPoints, 311, 370);
+            $('#game-finish-img2').attr('src', canvas2.toDataURL('image/jpeg'));
+
+            gameResult["curIMG"] = $('#game-finish-img').attr('src');
+            gameResult["curIMG2"] = $('#game-finish-img2').attr('src');
+        }
+    }
+
 });
 
 function checkErrors() {
